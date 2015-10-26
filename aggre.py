4a7936b88a1eb21353cb80a35af6d2e30948cda4
bk7749 <bk7749@gmail.com>
2015-10-25 22:19:26 -0700

    check

--- aggre.py
+++ aggre.py
@@ -15,10 +15,14 @@ class Aggregator():
 		self.bdDS = DataService(authdata.srcUrlBase, authdata.bdApiKey, authdata.bdUserName)
 		with open('metadata/doneuuidlist.csv', 'rb') as fp:
 			reader = csv.reader(fp, delimiter=';')
-			self.doneuuidlist = reader.readrow()
+			self.doneuuidList = list()
+			for row in reader:
+				self.doneuuidList = self.doneuuidList + row
 		with open('metadata/uuidlist.csv', 'rb') as fp:
 			reader = csv.reader(fp, delimiter=';')
-			self.uuidList= reader.readrow()
+			self.uuidList = list()
+			for row in reader:
+				self.uuidList = self.uuidList + row
 
 	def store_entire_uuids(self):
 		uuidList = list()
@@ -27,6 +31,7 @@ class Aggregator():
 			sensors = self.bdDS.list_sensors(query_context={}, offset=offset, limit = 1000)
 			sensors = sensors['sensors']
 			uuidList = uuidList + [sensor['uuid'] for sensor in sensors]
+		self.uuidList = uuidList
 		with open('metadata/uuidlist.csv', 'wb') as fp:
 			writer = csv.writer(fp, delimiter=';')
 			writer.writerow(uuidList)
@@ -37,23 +42,22 @@ class Aggregator():
 		maxuuidNumInFile = 200
 		dataDict = dict()
 		for uuid in self.uuidList:
-			if not uuid in self.doneList:
-				print fileIdx, uuidCnt, uuid
-				batchQ = dict()
-				sensorpoints = bdDS.list_sensorpoints(uuid, offset=0, limit=2)
-#				sensorpoints = sensorpoints['sensorpoints']
-#				pointsDict = dict()
-#				for sensorpoint in sensorpoints:
-#					pntName = sensorpoint['description']
-#					pointsDict[pntName] = {"start":beginTimeStr, "stop":endTimeStr}
-				batchQ[uuid] = {sensorpoint['description']: {'start':beginTimeStr, 'stop':endTimeStr} for sensorpoint in sensorpoints['sensorpoints']}
-				data = sefl.bdDS.get_timeseries_datapoints_batch(batchQ, timeout=100)
-				dataDict[data.keys()[0]] = data.values[0]
-				uuidCnt += 1
-				if uuidCnt >= maxuuidNumInFile:
-					uuidCnt = 0
-					with open(filename+str(fileIdx).n.zfill(3)+'.json', 'wb') as fp:
-						json.dump(dataDict, fp)
-					dataDict = dict()
-					fileIdx += 1
+			print fileIdx, uuidCnt, uuid
+			batchQ = dict()
+			sensorpoints = self.bdDS.list_sensorpoints(uuid, offset=0, limit=2)
+#			sensorpoints = sensorpoints['sensorpoints']
+#			pointsDict = dict()
+#			for sensorpoint in sensorpoints:
+#				pntName = sensorpoint['description']
+#				pointsDict[pntName] = {"start":beginTimeStr, "stop":endTimeStr}
+			batchQ[uuid] = {sensorpoint['description']: {'start':self.beginTimeStr, 'stop':self.endTimeStr} for sensorpoint in sensorpoints['sensorpoints']}
+			data = self.bdDS.get_timeseries_datapoints_batch(batchQ)
+			dataDict[data.keys()[0]] = data.values()[0]
+			uuidCnt += 1
+			if uuidCnt >= maxuuidNumInFile:
+				uuidCnt = 0
+				with open(filename+str(fileIdx).zfill(3)+'.json', 'wb') as fp:
+					json.dump(dataDict, fp)
+				dataDict = dict()
+				fileIdx += 1
 
--- top.py
+++ top.py
@@ -1,5 +1,7 @@
+import aggre
+reload(aggre)
 from aggre import Aggregator
 
 agg = Aggregator()
-agg.store_entire_uuids()
+#agg.store_entire_uuids()
 agg.store_entire_data()
